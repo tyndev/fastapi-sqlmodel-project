866f42861a6f92142bde3c9748655cba23fe29e1
tyndev <17089219+tyndev@users.noreply.github.com>
2024-02-27 19:26:20 -0800

    delete data

--- app.py
+++ app.py
@@ -143,10 +143,31 @@ def update_heroes():
         print("Does this refresh?", hero.name) # showing here that calling an attribute refreshes the whole object.
         print("Updated hero?>>>", hero)
 
+def delete_heroes():
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == "Spider-Youngster")
+        results = session.exec(statement)
+        hero = results.one()
+        print("Hero: ", hero)
+
+        session.delete(hero)
+        session.commit()
+
+        print("Deleted hero:", hero) # this shows that the data persists in the object in memory after deletion even though it has been deleted from the database. Refresh rasies an exception though because you have nothing to refresh with from the db. 
+
+        statement = select(Hero).where(Hero.name == "Spider-Youngster")
+        results = session.exec(statement)
+        hero = results.first()
+
+        if hero is None:
+            print("There's no hero named Spider-Youngster")
+
 def main():
     create_db_and_tables()
     create_heroes()
     select_heros()
     update_heroes()
+    delete_heroes()
+    
 if __name__ == "__main__":
     main()
