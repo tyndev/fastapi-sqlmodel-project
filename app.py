54981a76b1542728f5cc57a0be6fe1549b19c1ec
tyndev <17089219+tyndev@users.noreply.github.com>
2024-03-07 19:18:01 -0800

    add get a hero by id

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import asyncio
 from typing import List, Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from contextlib import asynccontextmanager
 
@@ -71,3 +71,14 @@ def read_heroes():
     with Session(engine) as session:
         heroes = session.exec(select(Hero)).all()
         return heroes
+    
+    
+# Define endpoint to read one hero via GET request
+@app.get("/heroes/{hero_id}", response_model=HeroRead)
+def read_hero(hero_id: int):
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        return hero
+    
